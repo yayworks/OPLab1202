75dba3ce95effb1310bc0d9e6013355b2754ca40
yayworks <raj@yayworks.com>
2017-09-17 15:27:45 -0500

    Update yb-sw-config.NIMBIX.x8664.turbotensor.sh

--- yb-sw-config.NIMBIX.x8664.turbotensor.sh
+++ yb-sw-config.NIMBIX.x8664.turbotensor.sh
@@ -11,10 +11,10 @@
 ####################################################################################################
 
 
-wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-ppc64le.sh
 
 (
-sudo bash Anaconda3-4.4.0-Linux-x86_64.sh <<EOF
+sudo bash Anaconda3-4.4.0-Linux-ppc64le.sh <<EOF
 
 yes
 /usr/local/anaconda3
@@ -43,22 +43,6 @@ EOF
 sudo /usr/local/anaconda3/envs/tensorflow/bin/pip install --upgrade prettytensor
 sudo /usr/local/anaconda3/envs/tensorflow/bin/pip install --upgrade gym
 
-cd /tmp
-wget https://s3.amazonaws.com/yb-lab-cfg/pnnl_tfg_v2.tar.gz
-tar xvfpz pnnl_tfg_v2.tar.gz
-###WORKDIR /root/cpu/py3.x
-
-export MPI_HOME=/usr
-export CUDA_HOME=/usr/local/cuda
-export CUDNN_HOME=/usr/local/cuda
-
-export PNETCDF_INSTALL_DIR=parallel-netcdf-1.7.0
-export TF_INSTALL_DIR=/tmp/gpu/py3.x
-export FAKE_SYSTEM_LIBS=$TF_INSTALL_DIR/fakeRoot/
-
-cd /tmp/gpu/py3.x
-source /tmp/gpu/py3.x/setAlias.sh
-source /tmp/gpu/py3.x/install_mpi_tf_cuda8_0.sh
 
 
 
